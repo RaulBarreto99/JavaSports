412f16e34efac2f422b148a90c6742b113b59ac7
Lucas Augusto <53840687+LuucasAugusto@users.noreply.github.com>
2020-05-07 18:42:43 -0300

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -29,6 +29,12 @@ create table "ROOT".FILIAL(
         GENERATED ALWAYS AS IDENTITY
         (START WITH 1, INCREMENT BY 1),
     NOME VARCHAR(60),
+);
+
+create table "ROOT".ENDERECO(
+	ID_FILIAL BIGINT not null primary key
+        GENERATED ALWAYS AS IDENTITY
+        (START WITH 1, INCREMENT BY 1),
     Bairro VARCHAR(60),
     CEP VARCHAR(60),
     PAIS VARCHAR(60),
