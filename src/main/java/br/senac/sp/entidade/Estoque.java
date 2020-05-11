35ad3f3835ef9e40e956805cd35aa13054f7157d
Daniel <daniel199257@gmail.com>
2020-05-10 21:13:51 -0300

    Alterando gráficos e listando produtos

---
+++ src/main/java/br/senac/sp/entidade/Estoque.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.sp.entidade;
+
+/**
+ *
+ * @author oz500
+ */
+public class Estoque {
+    
+    private int idProduto;
+    private String nomeProduto;
+    private String marcaProduto;
+    private double precoProduto;
+    private int quantidadeProduto;
+    
+    
+    public Estoque(String produto, String marca, double preco, int quantidade){
+        
+        this.nomeProduto = produto;
+        this.marcaProduto =  produto;
+        this.precoProduto = preco;
+        this.quantidadeProduto = quantidade;
+    }
+
+    public Estoque() {
+        
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getMarcaProduto() {
+        return marcaProduto;
+    }
+
+    public void setMarcaProduto(String marcaProduto) {
+        this.marcaProduto = marcaProduto;
+    }
+
+    public double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+
+    public int getQuantidadeProduto() {
+        return quantidadeProduto;
+    }
+
+    public void setQuantidadeProduto(int quantidadeProduto) {
+        this.quantidadeProduto = quantidadeProduto;
+    }
+    
+}
